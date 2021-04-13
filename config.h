ad63680a0404bbdde2c55f87f0c9a91ba46f2f78
Gondon Chan <gordon0226hotmail.com@gmail.com>
2021-04-13 12:49:37 +0800

    added swallow option

--- config.h
+++ config.h
@@ -7,11 +7,12 @@
 /* appearance */
 static const unsigned int borderpx    	= 1;        /* border pixel of windows */
 static const unsigned int snap        	= 32;       /* snap pixel */
+static const int swallowfloating    	= 1;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih        = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv        = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh        = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov        = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps              = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps              = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar              	= 1;        /* 0 means no bar */
 static const int topbar               	= 1;        /* 0 means bottom bar */
 static const int focusonwheel         	= 0;
@@ -66,13 +67,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	*/
-	/* class    instance      title       	 tags mask    isfloating   isterminal  monitor */
-	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,        -1 },
-	{ TERMCLASS,  NULL,       NULL,       	    0,            0,           1,        -1 },
-	{ NULL,       NULL,       "Event Tester",   0,            0,           0,        -1 },
-	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,        -1 },
-	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,        -1 },
-};
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	//{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+ };
 
 /* layout(s) */
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
